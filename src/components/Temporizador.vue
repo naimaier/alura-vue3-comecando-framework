fabe1aecd8a5c71256287145df0e092b7c7cbd9f
Henrique Naimaier <coubron@yahoo.com.br>
2023-05-23 11:57:52 -0300

    Managing button state

--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="is-flex is-align-items-center is-justify-content-space-between">
         <CronometroComp :tempoEmSegundos="tempoEmSegundos"/>
-        <button class="button" @click="iniciar">
+        <button class="button" @click="iniciar" :disabled="cronometroRodando">
             <span class="icon">
                 <i class="fas fa-play"></i>
             </span>
             <span>play</span>
         </button>
-        <button class="button" @click="finalizar">
+        <!-- : faz o atributo se likar com o estado da variavel -->
+        <button class="button" @click="finalizar" :disabled="!cronometroRodando">
             <span class="icon">
                 <i class="fas fa-stop"></i>
             </span>
@@ -24,17 +25,20 @@ export default defineComponent({
     data() {
         return {
             tempoEmSegundos: 0,
-            cronometro: 0
+            cronometro: 0,
+            cronometroRodando: false
         };
     },
     methods: {
         iniciar() {
+            this.cronometroRodando = true,
             // o setInterval retorna um id que representa esse interval
             this.cronometro = setInterval(() => {
                 this.tempoEmSegundos += 1; 
             }, 1000);
         },
         finalizar() {
+            this.cronometroRodando = false,
             clearInterval(this.cronometro);
         }
     },
